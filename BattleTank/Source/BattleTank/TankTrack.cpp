a70011978e9ef89ac64e1f160b8270b633b0e903
DarksiderRay <NG0007UI@e.ntu.edu.sg>
2017-03-18 21:33:03 +0800

    BT79 - OnComponentHit Event in 4.12

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -16,6 +16,16 @@ void UTankTrack::SetThrottle(float Throttle)
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm hit, I'm hit!"))
+}
+
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
