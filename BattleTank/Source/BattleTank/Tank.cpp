11ccbd6768c92d65e26b405bccdd34ec5df2af8d
DarksiderRay <NG0007UI@e.ntu.edu.sg>
2017-03-18 18:06:56 +0800

    BT71 - Using the ensure Assertion

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -21,6 +21,11 @@ ATank::ATank()
 
 void ATank::Fire()
 {
+	if (!ensure(Barrel))
+	{
+		return;
+	}
+
 	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
 
 	if (Barrel && isReloaded)
@@ -48,7 +53,7 @@ void ATank::BeginPlay()
 
 void ATank::AimAt(FVector HitLocation)
 {
-	if (!TankAimingComponent) return;
+	if (!ensure(TankAimingComponent)) return;
 
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
